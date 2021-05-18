3cb6cc5746d42bc6c3f0f841590b131105f2841c
Chester Wong <psyforce99@gmail.com>
2021-05-18 22:58:06 +0800

    Added theme and basic appbar/routing

--- spacebar/src/App.js
+++ spacebar/src/App.js
@@ -1,7 +1,49 @@
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import KanbanBoard from "./components/KanbanBoard";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#d5f4e6",
+      main: "#80ced6",
+      dark: "#618685"
+    },
+    secondary: {
+      main: '#fefbd8'
+    }
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: '400',
+    fontWeightRegular: '500',
+    fontWeightMedium: '600',
+    fontWeigthBold:'700'
+  }
+});
 
 function App() {
   return (
-    <div>Spacebar</div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AppBar color="primary">
+          <Toolbar variant="dense">
+            <Button color="inherit" href="/">
+              Home
+            </Button>
+            <Button color="inherit" href="/board">
+              Board
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/board" component={KanbanBoard} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
