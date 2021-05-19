7d59371a17cd462fdb6f684548fce29d5a96f421
Chester Wong <psyforce99@gmail.com>
2021-05-19 22:49:44 +0800

    Revert "Merge pull request #4 from Chesterwongz/ryan"
    This reverts commit a9574b531e7cd2bb54d41fd3de2b000e3f977ee3, reversing
    changes made to b7ff64d36bae2c0a790f06dfe168b01f3e9b46cc.

--- spacebar/src/App.js
+++ spacebar/src/App.js
@@ -1,58 +1,8 @@
-import React from 'react'; 
-import {Switch, Route} from 'react-router-dom'; 
-import {auth, createUserDocument} from './Firestore';
 
-import HomePage from './pages/HomePage/HomePage'; 
-import SignInPage from './pages/SignInPage/SignInPage'
-import NavBar from './Components/NavBar/NavBar';
-
-class App extends React.Component {
-constructor() {
-  super(); 
-  this.state = {
-    currentUser : null
-  }
-}
-
-unSubscribeFromAuth = null
-
-componentDidMount() {
-  this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    if (userAuth) {
-      const userRef = await createUserDocument(userAuth); 
-      userRef.onSnapshot(snapshot => {
-        this.setState({
-          currentUser: {
-            id: snapshot.id, 
-            ...snapshot.data()
-          }
-        }, () => {
-          console.log(this.state); 
-        })
-      })
-    } else {
-      this.setState({
-        currentUser: null
-      })
-    }
-  })
-}
-
-componentWillUnmount() {
-  this.unSubscribeFromAuth(); 
-}
-
-  render() {
-    return (
-      <div>
-        <NavBar currentUser = {this.state.currentUser}/>
-        <Switch>
-          <Route exact path = '/' component = {HomePage}/>
-          <Route path = '/signin' component = {SignInPage}/>
-        </Switch>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>Spacebar</div>
+  );
 }
 
 export default App;
