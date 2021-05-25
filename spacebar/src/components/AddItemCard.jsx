5a2701af0973df8ff581423cc74fcd2e41169826
Chester Wong <psyforce99@gmail.com>
2021-05-25 23:05:09 +0800

    made it possible to add and delete items

---
+++ spacebar/src/components/AddItemCard.jsx
@@ -0,0 +1,75 @@
+import { Button, IconButton, InputBase, Paper } from "@material-ui/core";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import ClearIcon from "@material-ui/icons/Clear";
+import React, { useState } from "react";
+import { db } from "../FireStore";
+import firebase from "firebase";
+import uuid from "react-uuid";
+
+const useStyle = makeStyles((theme) => ({
+  input: {
+    margin: theme.spacing(1),
+  },
+  btnConfirm: {
+    background: "#5AAC44",
+    color: "#fff",
+    "&:hover": {
+      background: fade("#5AAC44", 0.5),
+    },
+  },
+  confirm: {
+    margin: theme.spacing(1, 0, 0),
+  },
+}));
+export default function AddItemCard({ setOpen, listId }) {
+  const classes = useStyle();
+  const [title, setTitle] = useState("");
+
+  const addMoreCard = (title, listId) => {
+    db.collection("kanbanboard")
+      .doc(listId)
+      .update({
+        items: firebase.firestore.FieldValue.arrayUnion({
+          id: `${uuid()}`,
+          title: title,
+        }),
+      });
+  };
+
+  const handleOnChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleBtnConfirm = () => {
+    addMoreCard(title, listId);
+    setTitle("");
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div>
+        <Paper>
+          <InputBase
+            onChange={handleOnChange}
+            multiline
+            onBlur={() => setOpen(false)}
+            fullWidth
+            inputProps={{
+              className: classes.input,
+            }}
+            value={title}
+            placeholder={"Enter a title of this card.."}
+          />
+        </Paper>
+      </div>
+      <div className={classes.confirm}>
+        <Button className={classes.btnConfirm} onClick={handleBtnConfirm}>
+          {"Add Card"}
+        </Button>
+        <IconButton onClick={() => setOpen(false)}>
+          <ClearIcon />
+        </IconButton>
+      </div>
+    </>
+  );
+}
--- spacebar/src/components/KanbanList.jsx
+++ spacebar/src/components/KanbanList.jsx
@@ -2,6 +2,7 @@ import { Button, makeStyles, Paper, Typography } from "@material-ui/core";
 import React from "react";
 import { Droppable } from "react-beautiful-dnd";
 import ItemCard from "./ItemCard.jsx";
+import AddItemContainer from "./AddItemContainer.jsx";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,8 +25,6 @@ const useStyles = makeStyles((theme) => ({
 export default function KanbanList({ list }) {
   const classes = useStyles();
 
-  const handleClick = () => {};
-
   return (
     <div>
       <Paper className={classes.paper}>
@@ -34,16 +33,18 @@ export default function KanbanList({ list }) {
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
               {list.items.map((item, index) => (
-                <ItemCard key={item.id} item={item} index={index} />
+                <ItemCard
+                  key={item.id}
+                  item={item}
+                  listId={list.id}
+                  index={index}
+                />
               ))}
               <div className={classes.item}>{provided.placeholder}</div>
             </div>
           )}
         </Droppable>
-
-        <Button className={classes.addItem} onClick={handleClick}>
-          + Add Item
-        </Button>
+        <AddItemContainer listId={list.id} />
       </Paper>
     </div>
   );
